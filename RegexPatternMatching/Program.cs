dda1a88f0e7a7b1fd52d1901d34a7ff82aed342b
maha <mmahaece072gmail.com>
2021-11-11 17:37:14 +0530

    Add project files.

---
+++ RegexPatternMatching/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ValidationUsingRegex
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegexUserRegistration regex  = new RegexUserRegistration();
+            bool validResult = regex.ValidateFirstName();
+            bool validLastNameResult = regex.ValidateLastName();
+            bool validMobileResult = regex.ValideMobileNumber();
+            bool validEmailResult = regex.ValidateEmail();
+            bool validPasswordResult = regex.ValidatePassword();
+
+            Console.WriteLine("Firstname" +" "+ validResult);
+            Console.WriteLine("Lastname" +" "+validLastNameResult);
+            Console.WriteLine("Mobilenumber" +" "+validMobileResult);
+            Console.WriteLine("Email" +" "+validEmailResult);
+            Console.WriteLine("Password" +" "+validPasswordResult);
+        }
+    }
+}
---
+++ RegexPatternMatching/RegexUserRegistration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ValidationUsingRegex
+{
+    class RegexUserRegistration
+
+    {
+
+        public static string NAME_REGEX = "^[A-Z]{1}[A-Za-z]{3,}$";
+        public static string MOBILENUMBER_REGEX = "^[6-9]{1}[0-9]{9}$";
+        public static string EMAIL_REGEX = "^[0-9a-zA-z]+[.+-_][0,1}[0-9a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}";
+        public static string PASSWORD_REGEX = "^[a-zA-Z0-9]{8,}[&%$@]{1}";
+
+
+        public bool ValidateFirstName()
+        {
+            return Regex.IsMatch("Maha", NAME_REGEX);
+        }
+
+        public bool ValidateLastName()
+        {
+            return Regex.IsMatch("Lakshmi", NAME_REGEX);
+        }
+
+        public bool ValideMobileNumber()
+        {
+            return Regex.IsMatch("9123456781", MOBILENUMBER_REGEX);
+        }
+        public bool ValidateEmail()
+        {
+            return Regex.IsMatch("abc.XYZ@bl.co.in", EMAIL_REGEX);
+        }
+        public bool ValidatePassword()
+        {
+            return Regex.IsMatch("asZ2d34f$", PASSWORD_REGEX);
+        }
+    }
+}
